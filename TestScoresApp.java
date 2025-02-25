7ce6b69a5228199b5b52ff274ad2fe8359475d12
Farah <fara734isgreat@gmail.com>
2025-02-25 11:54:18 +0300

    TestScoresApp

--- TestScoresApp.java
+++ TestScoresApp.java
@@ -1,4 +1,4 @@
-//فرح خليل حسن طبل
+
 package farah;
 import java.util.Scanner;
 public class TestScoresApp {
@@ -6,7 +6,6 @@ public class TestScoresApp {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Ask the user to enter three test scores
         System.out.print("Enter the first test score: ");
         double score1 = scanner.nextDouble();
 
@@ -16,14 +15,11 @@ public class TestScoresApp {
         System.out.print("Enter the third test score: ");
         double score3 = scanner.nextDouble();
 
-        // Create a TestScores object
         TestScores testScores = new TestScores(score1, score2, score3);
 
-        // Calculate and display the average of the test scores
         double average = testScores.calculateAverage();
         System.out.println("\nThe average of the test scores is: " + average);
 
-        scanner.close();
     }
 }    
 
