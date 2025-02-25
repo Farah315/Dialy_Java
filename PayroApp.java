19f379c58f7323e9d01d4bd4c07b9a7fed2c2dc0
Farah <fara734isgreat@gmail.com>
2025-02-25 11:39:18 +0300

    Payroll-App

--- PayroApp.java
+++ PayroApp.java
@@ -1,10 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package farah;
 import java.util.Scanner;
-//فرح خليل حسن طبل
+
 public class PayroApp {
     public static void main(String[] args) {
         Scanner s= new Scanner(System.in);
