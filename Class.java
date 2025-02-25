c59eae04d6965daa1b000c90b7cbac5efeb63791
Farah <fara734isgreat@gmail.com>
2025-02-25 11:34:21 +0300

    Pets

--- Class.java
+++ Class.java
@@ -1,20 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package pkgclass;
 
-/**
- *
- * @author Al-Qema
- */
 public class Class {
 
     public static void main(String[] args) {
         Pet myPet = new Pet();
 
-        myPet.setName("Buddy");
-        myPet.setAnimal("Dog");
+        myPet.setName("Vic");
+        myPet.setAnimal("cat");
         myPet.setAge(5);
         System.out.println("Pet Name: " + myPet.getName());
         System.out.println("Pet Animal: " + myPet.getAnimal());
