3c1b2c474abcad7d58c6c338de45aa8a4053a752
Farah <fara734isgreat@gmail.com>
2025-02-25 11:35:13 +0300

    Employee_App

--- Employee_App.java
+++ Employee_App.java
@@ -1,9 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package pkgclass;
-//فرح خليل حسن طبل
 public class Employee_App {
     public static void main(String[] args) {
      
