b3f6772afd8f388b02dfca6af79f421e4ad36ba9
Farah <fara734isgreat@gmail.com>
2025-02-25 11:34:52 +0300

    Employee

--- Employee.java
+++ Employee.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package pkgclass;
 
-/**
- *
- * @author Al-Qema
- *///فرح خليل حسن طبل
 public class Employee {
     private String name;
     private int idNumber;
