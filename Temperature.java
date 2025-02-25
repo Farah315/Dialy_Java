21845a6c124a457b6a79fd4d758bf635b37c62b4
Farah <fara734isgreat@gmail.com>
2025-02-25 11:48:54 +0300

    Update Temperature.java

--- Temperature.java
+++ Temperature.java
@@ -1,10 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package farah;
-//فرح خليل حسن طبل
 
+package farah;
 public class Temperature {
     private double ftemp;  
     public Temperature(double f) {
