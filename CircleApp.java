f6b345e67cacfaebf118440c0afe4ca0be30bf40
Farah <fara734isgreat@gmail.com>
2025-02-25 11:29:13 +0300

    CircleApp

--- CircleApp.java
+++ CircleApp.java
@@ -1,9 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package farah;
-//فرح خليل حسن طبل
+
 
 import java.util.Scanner;
 
